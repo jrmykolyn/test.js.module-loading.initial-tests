b7450837dd83b765742a03da71cd63be742a8328
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-01-27 22:14:37 -0500

    CommonJS - Added new proof-of-concept method to 'string-helper' module.

--- commonjs/index.js
+++ commonjs/index.js
@@ -1,3 +1,5 @@
 var stringHelper = require( './string-helper' );
 
-console.log( stringHelper.upper( 'my string' ) );
+console.log( stringHelper.upper( 'this string was initial lower case.' ) );
+
+console.log( stringHelper.lower( 'THIS STRING WAS INITIALLY UPPER CASE.' ) );
--- commonjs/string-helper/index.js
+++ commonjs/string-helper/index.js
@@ -7,7 +7,16 @@ function upper( str ) {
     return str.toUpperCase();
 }
 
+function lower( str ) {
+    if ( typeof str !== 'string' ) {
+        return str;
+    }
+
+    return str.toLowerCase();
+}
+
 // Return public API.
 module.exports = {
-    upper: upper
+    upper: upper,
+    lower: lower
 };
