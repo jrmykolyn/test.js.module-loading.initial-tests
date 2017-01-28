aeb2f33926b29e2119d6e6004a08c66f4431e514
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-01-28 13:12:32 -0500

    SystemJS - Completed additional tests re: ES6-style module imports, transpiling down to AMD.

---
+++ systemjs/src/js/app/_db-module.js
@@ -0,0 +1,13 @@
+export function connect( options ) {
+
+    console.log( 'INSIDE `connect`' );
+
+    return 1;
+}
+
+export function disconnect( options ) {
+
+    console.log( 'INSIDE `disconnect`' );
+
+    return 0;
+}
--- systemjs/src/js/app/app.js
+++ systemjs/src/js/app/app.js
@@ -1,6 +1,11 @@
+import * as db from 'app/_db-module';
+
 export function init() {
 
     console.log( 'INSIDE THE `init()` FUNCTION' );
+
+    db.connect();
+    db.disconnect();
 
     return 1;
 }
